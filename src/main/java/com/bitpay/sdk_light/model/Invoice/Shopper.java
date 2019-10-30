71d5541191dc8568cb4a179607b1b270ac878549
Antonio Buedo <sales-engineering@bitpay.com>
2019-10-30 16:06:47 +0100

    - Invoice model extended

---
+++ src/main/java/com/bitpay/sdk_light/model/Invoice/Shopper.java
@@ -0,0 +1,24 @@
+package com.bitpay.sdk_light.model.Invoice;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class Shopper {
+
+    private String _user;
+
+    public Shopper() {
+    }
+
+    @JsonIgnore
+    public String getName() {
+        return _user;
+    }
+
+    @JsonProperty("user")
+    public void setName(String user) {
+        this._user = user;
+    }
+}
